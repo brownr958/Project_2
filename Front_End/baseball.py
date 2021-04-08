cc46fbf7bb31aa8f36564d733bb1cb5ef6b8d748
unknown <tarmonkey178@aim.com>
2021-04-08 19:30:41 -0400

    updated py

--- Front_End/baseball.py
+++ Front_End/baseball.py
@@ -7,7 +7,7 @@ import pymongo
 app = Flask(__name__)
 
 # Create connection variable
-conn = "mongodb+srv://thebaseballers:password@cluster0.6hxjz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+conn = "mongodb+srv://thebaseballers:project2@cluster0.6hxjz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 # Pass connection to the pymongo instance.
 client = pymongo.MongoClient(conn)
@@ -21,14 +21,51 @@ all_data = db.all_data
 
 
 # Set route
-@app.route('/')
+@app.route('/allStats')
 def index():
     # Store the entire team collection in a list
     data = list(all_data.find())
+    data_json = []
+    for item in data:
+        row = {}
+        row['AB'] = item['AB']
+        row['AVG'] = item['AVG']
+        row['Age'] = item['Age']
+        row['H'] = item['H']
+        row['HR'] = item['HR']
+        row['Name'] = item['Name']
+        row['OBP'] = item['OBP']
+        row['OPS'] = item['OPS']
+        row['Off'] = item['Off']
+        row['Position'] = item['Position']
+        row['R'] = item['R']
+        row['RBI'] = item['RBI']
+        row['SB'] = item['SB']
+        row['SLG'] = item['SLG']
+        row['Season'] = item['Season']
+        row['Team'] = item['Team']
+        row['WAR'] = item['WAR']
+        row['playerid'] = item['playerid']
+        
+        data_json.append(row)
     
-
     # Return the template with the teams list passed in
-    return render_template('baseball.html', data=data)
+    return jsonify(data_json)
+
+@app.route('/WAR')
+def index():
+    # Store the entire team collection in a list
+    data = list(all_data.find())
+    data_json = []
+    for item in data:
+        row = {}
+        row['Season'] = item['Season']
+        row['WAR'] = item['WAR']
+        
+        data_json.append(row)
+    
+    # Return the template with the teams list passed in
+    return jsonify(data_json)
 
 
 
